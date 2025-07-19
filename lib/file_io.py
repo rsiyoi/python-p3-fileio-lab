f36e221675f7b081dcc8d01febca0ef90f2879b2
Ruth Siyoi <ruth.siyoi@gmail.com>
2025-07-19 12:11:17 +0300

    file io

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode='w') as log_file:
+        log_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode='a') as log_file:
+        log_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding='utf-8') as text_file:
+        return text_file.read()
+
